ef1a5544d19ca4fad8598631fb8533c6cee5dfb4
Davinder Singh Brar <brads001@mymail.unisa.edu.au>
2020-02-18 10:39:34 +1030

    added form

---
+++ js/simplex.js
@@ -0,0 +1,8 @@
+// const inputs = document.getElementsByClassName("input");
+// function dim() {
+//   this.style.marginTop = "10px";
+// }
+
+// for (let i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener("focus", dim);
+// }
